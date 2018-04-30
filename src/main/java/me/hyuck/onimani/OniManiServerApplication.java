691784824241a9883f716c45ef77ad1c5913de46
Hyuck9 <lhg1304@gmail.com>
2018-04-30 17:03:58 +0900

    Initial commit

---
+++ src/main/java/me/hyuck/onimani/OniManiServerApplication.java
@@ -0,0 +1,12 @@
+package me.hyuck.onimani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OniManiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OniManiServerApplication.class, args);
+    }
+}
---
+++ src/test/java/me/hyuck/onimani/OniManiServerApplicationTests.java
@@ -0,0 +1,16 @@
+package me.hyuck.onimani;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OniManiServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
